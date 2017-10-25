ffae6367872b5b37b553a1e315677bb5f63e50d7
Eugene Kirdzei <e.ki@nuwber.com>
2017-10-25 13:45:50 +0300

    Add support for pcntl signals

--- src/ProcessingOptions.php
+++ src/ProcessingOptions.php
@@ -12,10 +12,15 @@ class ProcessingOptions
      * @var int
      */
     public $maxTries;
+    /**
+     * @var int
+     */
+    public $memory;
 
-    public function __construct($timeout = 60, $maxTries = 0)
+    public function __construct($memory = 128, $timeout = 60, $maxTries = 0)
     {
         $this->timeout = $timeout;
+        $this->memory = $memory;
         $this->maxTries = (int)$maxTries;
     }
 }
