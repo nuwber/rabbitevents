0ee2bd51e696362af69274a78cb7f606ebba09e0
Eugene Kirdzei <masterjus@gmail.com>
2019-12-31 11:38:55 +0300

    Fix typo

--- src/BroadcastEventServiceProvider.php
+++ src/BroadcastEventServiceProvider.php
@@ -4,7 +4,7 @@ namespace Nuwber\Events;
 
 /**
  * @deprecated
- * Left just for back compatibility and will be removed in text releases
+ * Left just for back compatibility and will be removed in next releases
  */
 class BroadcastEventServiceProvider extends RabbitEventsServiceProvider
 {
