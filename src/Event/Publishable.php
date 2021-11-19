9b212aa9741f674f0f1762809e5068119b40c645
Nicholas Bates <nicholas@nrobates.com>
2021-11-18 17:39:37 -0800

    Re-adding testing fake

--- src/Event/Publishable.php
+++ src/Event/Publishable.php
@@ -2,14 +2,47 @@
 
 namespace Nuwber\Events\Event;
 
+use Illuminate\Container\Container;
+
 trait Publishable
 {
-    /**
-     * @return mixed
-     * @throws \Throwable
-     */
+    protected static $recording = false;
+
+    public static function fake()
+    {
+        static::$recording = true;
+
+        Container::getInstance()->instance(Publisher::class, \Mockery::spy(Publisher::class));
+    }
+
+    public static function assertPublished(string $event, array $payload)
+    {
+        Container::getInstance()
+            ->make(Publisher::class)
+            ->shouldHaveReceived()
+            ->publish(\Mockery::on(function (ShouldPublish $object) use ($event, $payload) {
+                return $object instanceof static
+                    && $object->publishEventKey() == $event
+                    && $object->toPublish() == $payload;
+            }))
+            ->once();
+    }
+
+    public static function assertNotPublished()
+    {
+        Container::getInstance()
+            ->make(Publisher::class)
+            ->shouldNotHaveReceived()
+            ->publish(\Mockery::type(static::class));
+    }
+
     public static function publish(): void
     {
-        publish(new static(...func_get_args()));
+        Container::getInstance()
+            ->make(Publisher::class)
+            ->publish(
+                new static(...func_get_args())
+            );
     }
 }
+
