075a1259bc89582634a6f3b71f72ab6718892c49
Eugene Nuwber <142213350+eugene-nuwber@users.noreply.github.com>
2023-10-16 10:27:09 +0200

    Issue #121: Fix queue name max length (#122)
    * Issue #121: Fix queue name max length
    
    * Add tests and doc
    
    * Do same check as AMQP driver

--- src/RabbitEvents/Foundation/Support/EnqueueOptions.php
+++ src/RabbitEvents/Foundation/Support/EnqueueOptions.php
@@ -17,6 +17,12 @@ class EnqueueOptions implements QueueNameInterface
 
     public function resolveQueueName(): string
     {
-        return $this->applicationName . ":" . implode(',', $this->events);
+        $name = $this->applicationName . ':' . implode(',', $this->events);
+
+        if (mb_strlen($name, 'ASCII') > 255) {
+            $name = $this->applicationName . ':' . md5($name);
+        }
+
+        return $name;
     }
 }
