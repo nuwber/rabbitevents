a9579724b8bc4a7153351da080d4fb9eae744fb8
noith <noith@yandex.ru>
2025-03-25 02:16:34 +0300

    Small fixes

--- src/RabbitEvents/Listener/Commands/ListenCommand.php
+++ src/RabbitEvents/Listener/Commands/ListenCommand.php
@@ -41,7 +41,7 @@ class ListenCommand extends Command
                             {--tries=1 : Number of times to attempt to handle a Message before logging it failed}
                             {--sleep=5 : Sleep time in seconds before handling failed message next time}
                             {--quiet: No console output}
-                            {--max-priority: If you use prioritised queues, set max priority option, or it will be used from config}';
+                            {--max-priority= : If you use prioritised queues, set max priority option, or it will be used from config}';
 
     /**
      * The console command description.
@@ -107,7 +107,10 @@ class ListenCommand extends Command
     public function generateArguments(): array
     {
         $args = [];
-        $maxPriority = $this->option('max-priority') ?? $this->laravel['config']['rabbitevents.max_priority'];
+        $maxPriority = $this->option('max-priority');
+        if(empty($maxPriority)){
+            $maxPriority = $this->laravel['config']['rabbitevents.max_priority'];
+        }
         if (!empty($maxPriority)) {
             $args['x-max-priority'] = $maxPriority;
         }
