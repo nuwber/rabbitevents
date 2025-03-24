4c8cb47f9c03eb33b91f108ccadf72f399c7e958
noith <noith@yandex.ru>
2025-03-24 21:14:57 +0300

    AfterCommit publishing messages

--- src/RabbitEvents/Publisher/Publisher.php
+++ src/RabbitEvents/Publisher/Publisher.php
@@ -4,23 +4,52 @@ declare(strict_types=1);
 
 namespace RabbitEvents\Publisher;
 
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Database\DatabaseTransactionsManager;
 use RabbitEvents\Foundation\Contracts\Transport;
+use RabbitEvents\Publisher\Support\AfterCommitMessageInterface;
 
 class Publisher
 {
     public function __construct(
         private MessageFactory $messageFactory,
-        private Transport $transport
-    ) {
+        private Transport      $transport
+    )
+    {
+    }
+
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function publish(ShouldPublish $event): void
+    {
+        $manager = $this->resolveTransactionManager();
+        if ($event instanceof AfterCommitMessageInterface && !is_null($manager)) {
+            /** @var ShouldPublish $event */
+            $manager->addCallback(
+                fn() => $this->send($event)
+            );
+        } else {
+            $this->send($event);
+        }
     }
 
     /**
-     * Publish event to
+     * Get the database transaction manager implementation from the resolver.
      *
-     * @param ShouldPublish $event
-     * @return void
+     * @return DatabaseTransactionsManager|null
+     * @throws BindingResolutionException
      */
-    public function publish(ShouldPublish $event): void
+    protected function resolveTransactionManager(): ?DatabaseTransactionsManager
+    {
+        return Container::getInstance()->bound('db.transactions')
+            ? Container::getInstance()->make('db.transactions')
+            : null;
+    }
+
+    protected function send(ShouldPublish $event): void
     {
         $this->transport->send(
             $this->messageFactory->make($event)
